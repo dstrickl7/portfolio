b3c2f626a738ea38c3aece2c793f0971917ac918
Danielle S <strickl6@illinois.edu>
2021-08-18 13:54:31 -0500

    1024px layout - project card layout

--- scripts.js
+++ scripts.js
@@ -1,6 +1,10 @@
 const navbar = document.querySelector(".navbar").classList;
 const close = document.getElementById("close");
 const hamburger = document.getElementById("hamburger");
+const project1 = document.getElementById("project1");
+const project2 = document.getElementById("project2");
+const project3 = document.getElementById("project3");
+const project4 = document.getElementById("project4");
 
 hamburger.addEventListener("click", () => {
   navbar.toggle("active");
@@ -13,3 +17,15 @@ close.addEventListener("click", () => {
   close.classList.toggle("active");
   hamburger.classList.toggle("active");
 });
+
+if (document.documentElement.clientWidth > 600) {
+  project1.src = "styles/Images/desktop/room-homepage.png";
+  project2.src = "styles/Images/desktop/loopstudios-desktop.png";
+  // project3.src=;
+  // project4.src=;
+} else {
+  project1.src = "styles/Images/mobile/room-homepage-mobile.png";
+  project2.src = "styles/Images/mobile/loopstudios-mobile.png";
+  // project3.src=;
+  // project4.src=;
+}
