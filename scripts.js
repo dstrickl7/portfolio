a78f77327402f270ea839af75da00e662c42be44
Danielle S <strickl6@illinois.edu>
2021-08-21 14:24:53 -0500

    fixes for deployment, adjusting color scheme for accessibility

--- scripts.js
+++ scripts.js
@@ -1,10 +1,7 @@
+/*Navigation menu */
 const navbar = document.querySelector(".navbar").classList;
 const close = document.getElementById("close");
 const hamburger = document.getElementById("hamburger");
-const project1 = document.getElementById("project1");
-const project2 = document.getElementById("project2");
-const project3 = document.getElementById("project3");
-const project4 = document.getElementById("project4");
 
 hamburger.addEventListener("click", () => {
   navbar.toggle("active");
@@ -18,6 +15,13 @@ close.addEventListener("click", () => {
   hamburger.classList.toggle("active");
 });
 
+/*Chooses image based on screen width */
+
+const project1 = document.getElementById("project1");
+const project2 = document.getElementById("project2");
+const project3 = document.getElementById("project3");
+const project4 = document.getElementById("project4");
+
 if (document.documentElement.clientWidth > 600) {
   project1.src = "styles/Images/desktop/room-homepage.png";
   project2.src = "styles/Images/desktop/loopstudios-desktop.png";
@@ -29,3 +33,30 @@ if (document.documentElement.clientWidth > 600) {
   // project3.src=;
   project4.src = "styles/Images/mobile/portfolio-mobile.png";
 }
+
+/*Form validation*/
+
+const email = document.getElementById("email");
+const name = document.getElementById("name");
+
+name.addEventListener("invalid", () => {
+  if (name.value === "") {
+    name.setCustomValidity("Don't be shy! Enter your first name.");
+  } else if (name.validity.tooShort) {
+    name.setCustomValidity(
+      "Please enter a valid name longer than 2 characters"
+    );
+  } else {
+    name.setCustomValidity("No numbers or symbols please!");
+  }
+});
+
+email.addEventListener("invalid", () => {
+  if (email.value === "") {
+    email.setCustomValidity("Please enter an email address");
+  } else if (email.typeMismatch) {
+    email.setCustomValidity(
+      "Please enter a valid email in the format xxx@xx.xxx"
+    );
+  }
+});
